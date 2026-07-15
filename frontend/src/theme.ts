4d03e316e8215f60e1a63de5adc198ff6d5364dc
Sushant Tickoo <63288060+sushanttickoo22@users.noreply.github.com>
2026-07-15 13:35:38 +0530

    Create theme.ts

---
+++ frontend/src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#1565C0",
+        },
+        secondary: {
+            main: "#2E7D32",
+        },
+    },
+});
+
+export default theme;
