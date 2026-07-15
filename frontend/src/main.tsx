346b733ccc528e12fd3fc1e5fec67362ce31fbf0
Sushant Tickoo <63288060+sushanttickoo22@users.noreply.github.com>
2026-07-15 13:35:57 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
+import theme from "./theme";
 import App from "./App";
 
 ReactDOM.createRoot(
     document.getElementById("root")!
 ).render(
-
     <React.StrictMode>
-
-        <App />
-
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
-
 );
