c8017b829e4bbd3e38bbe25739dbfc698473a968
Sushant Tickoo <63288060+sushanttickoo22@users.noreply.github.com>
2026-07-15 13:32:37 +0530

    Create Upload.tsx

---
+++ frontend/src/pages/Upload.tsx
@@ -0,0 +1,17 @@
+export default function Upload() {
+
+    return (
+        <div>
+
+            <h1>Upload Electoral Roll</h1>
+
+            <p>
+
+                PDF Upload UI will be added in Batch 7.
+
+            </p>
+
+        </div>
+    );
+
+}
