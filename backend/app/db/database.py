b86f8a211cedc966746dbcb00cc4eb6e7f8c96a4
Sushant Tickoo <63288060+sushanttickoo22@users.noreply.github.com>
2026-07-15 13:16:51 +0530

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,17 +4,18 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 from app.db.base import Base
 
+# Import all models here
+from app.models.uploaded_file import UploadedFile
+
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    }
+    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(
     bind=engine,
     autoflush=False,
-    autocommit=False
+    autocommit=False,
 )
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
