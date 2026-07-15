418d6748a3efa82451ff0fb8e638859ecc39aeae
Sushant Tickoo <63288060+sushanttickoo22@users.noreply.github.com>
2026-07-15 13:40:32 +0530

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,3 @@
 export default function Dashboard() {
-    return (
-        <div>
-            <h1>ERIS Dashboard</h1>
-
-            <p>
-                Welcome to Election Roll Intelligence System
-            </p>
-        </div>
-    );
+    return <h2>Dashboard</h2>;
 }
